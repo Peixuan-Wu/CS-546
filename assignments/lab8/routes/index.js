faafd8c67b42ec68295d771fd06e8ff4340376a5
Peixuan WU <ward00ani@gmail.com>
2022-11-15 22:08:41 -0500

    update

--- assignments/lab8/routes/index.js
+++ assignments/lab8/routes/index.js
@@ -5,7 +5,7 @@ const peopleRoutes = require('./people');
 const constructorMethod = (app) => {
   app.use('/', peopleRoutes);
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve('./static/homepage.html'));
+    res.sendFile(path.resolve('static/homepage.html'));
   });
   app.use('*', (req, res) => {
     res.sendStatus(404);
